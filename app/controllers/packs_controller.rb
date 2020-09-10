4a12dd416dfb0e8008916a012c10ad1a5e9af042
sbrook13 <sbrook13@gmail.com>
2020-09-10 12:02:17 -0600

    updated controller methods with correct links

--- app/controllers/packs_controller.rb
+++ app/controllers/packs_controller.rb
@@ -1,2 +1,44 @@
 class PacksController < ApplicationController
+    def index
+        @packs = Pack.all
+        render json: @packs
+    end
+
+    def show
+        @pack = Pack.find(params[:id])
+        render json: @pack
+    end
+
+    def create
+        @pack = Pack.create(
+            user_id: params[:user_id],
+            name: params[:name]
+        )
+
+        params[:gear_ids].each do |gear_id|
+            SelectedGear.create(
+                pack_id: @pack.id,
+                gear_id: gear_id
+            )
+        end
+        redirect_to 'http://localhost:3001/profile.html?user_id=#{params[:user_id]}'
+    end
+
+    def update
+        @pack = Pack.find(params[:id])
+
+        @pack.update({
+            name: params[:name]
+        })
+
+        redirect_to 'http://localhost:3001/profile.html?user_id=#{@pack.user_id}'
+    end
+
+    def destroy
+        @pack = Pack.find(params[:id])
+        @pack.destroy
+
+        redirect_to 'http://localhost:3001/profile.html?user_id=#{@pack.user_id}'
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :packs
     has_many :selected_gears, through: :packs
+
+    validates :username, uniqueness: true
 end
